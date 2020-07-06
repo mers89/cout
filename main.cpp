01b85fbc3e4ab0019f84ad7eef71ed64fb995396
MercyM <mercymanrique@yahoo.com>
2020-07-06 01:02:37 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+-program purpose: program that displays in block form something you did or happened to you over the break
+-input: n/a
+-output: print message to stdout
+*/
+
+//Including library for function cout
+//Function purpose: header file, tells processor to include the iostream standard input/output stream library
+#include<iostream>
+//using namespace for the program
+//Function purpose: tells program to use cout as define on the default library of iostream
+using namespace std;
+//Including text to be displayed as block and run in program function
+/*Function purpose: uses object defined in header file iostream (cout) and displays what is given in standard 
+output stream in this case monitor screen and keeps arrangement of characters in respect to spaces and new lines*/
+//parameters: not used
+int main()
+//Input
+{
+cout<<"NN  N  EEEE  W W W     CCCC     A     RRRRR   !"<<endl;
+cout<<"NN  N  E     W W W     C       A A    R   R   !"<<endl;
+cout<<"N N N  EEEE  W W W     C      AAAAA   RRRRR   !"<<endl;
+cout<<"N  NN  E     W W W     C     A     A  R  R      "<<endl;
+cout<<"N   N  EEEE  WWWWW     CCCC A       A R   R  !!!"<<endl;
+}
+//return values: not used
+//identifier: not used
+
